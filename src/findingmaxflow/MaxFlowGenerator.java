31deb595d80682caed7baeb2aa789cf4e8a0eb01
YeshengC <yesheng250416@gmail.com>
2019-12-05 19:37:49 -0600

    dot to png fixed

--- src/findingmaxflow/MaxFlowGenerator.java
+++ src/findingmaxflow/MaxFlowGenerator.java
@@ -1,4 +1,10 @@
 package findingmaxflow;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 // sources: we use and modify edmonds-karp algorithm from
@@ -104,16 +110,56 @@ public class MaxFlowGenerator {
         } 
     } 
     
+    public static String maxdot (int[][] mGraph, int[][] graph, int num, int maxflow) throws FileNotFoundException, UnsupportedEncodingException {
+		String name = "maxgraph"+num+".dot";
+		PrintWriter writer = new PrintWriter(name, "UTF-8");
+        writer.println("digraph g{");
+        writer.println();
+        writer.println("rankdir = LR");
+        int len = graph.length;
+        for (int i = 0; i < len; i++) {
+        	for (int j = 0; j < len; j++) {
+        		if(mGraph[i][j]!=0) {
+        			writer.println(i + " -> " + j + " [label = \" " + mGraph[i][j]+"/"+graph[i][j]+ " \"];");
+        		}
+        	}
+        }
+        writer.println();
+        writer.println("label = " + "\" maxgraph "+num+":  maximum flow = "+maxflow+" \"");
+        writer.println("}");
+        writer.close();
+        return name;
+    }
+    
     // for testing
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException, InterruptedException {
+    	//generating flow network
+    	for(int i=0; i<10; i++) {
     	FlowNetworkGenerator flow = new FlowNetworkGenerator();
-    	flow.print(flow.graph);
-    	System.out.println();
+    	
+    	//generating flow network dot file
+    	String file=flow.dotfile(flow.graph,i);
+    	//preparing for png file
+    	ProcessBuilder processBuilder = new ProcessBuilder();
+    	String[] command = {"/bin/bash", "-c", "/usr/local/bin/dot -Tpng "+file+" -o "+file+".png"};
+    	processBuilder.command(command);
+    	//generating png from dot file 
+    	Process process = processBuilder.start();
+    	
+    	//generating max flow network
     	MaxFlowGenerator max = new MaxFlowGenerator(flow);
     	max.maxflow();
-    	flow.print(max.mGraph);
-    	System.out.println();
-    	System.out.println(max.maxFlow);
+
+    	//generating max flow dot file 
+    	String maxfile = max.maxdot(max.mGraph,flow.graph,i,max.maxFlow);
+    	
+    	//preparing for png file
+    	String[] command2 = {"/bin/bash", "-c", "/usr/local/bin/dot -Tpng "+maxfile+" -o "+maxfile+".png"};
+    	processBuilder.command(command2);
+    	
+    	//generating png from dot file 
+    	Process process2 = processBuilder.start();
+    	}
     }
 }
 
